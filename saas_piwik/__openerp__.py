78702ca4e98cb13719738c9dc78249ee9b856def
diassynthesis <diassynthesis@gmail.com>
2014-12-26 18:50:56 +0100

    Update __openerp__.py

--- saas_piwik/__openerp__.py
+++ saas_piwik/__openerp__.py
@@ -27,10 +27,7 @@
     'author': 'Yannick Buron',
     'license': 'AGPL-3',
     'website': 'https://github.com/YannickB',
-    'description': """
-    SaaS Piwik
-    =================
-    """,
+    'description': """SaaS Piwik""",
     'demo': [],
     'data': ['saas_piwik_data.xml'],
     'installable': True,
